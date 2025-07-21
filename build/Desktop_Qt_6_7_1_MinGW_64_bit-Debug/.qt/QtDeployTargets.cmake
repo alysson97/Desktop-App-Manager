7fd3c6a4ef9f34336ce486ead9165f1b417f6253
alysson <alyssonfvictoria@gmail.com>
2025-07-20 21:07:52 -0300

    Remover pasta build do controle de versão

--- build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_DashboardApp_FILE C:/Users/aly_v/Scripts_c++/DashboardApp/build/Desktop_Qt_6_7_1_MinGW_64_bit-Debug/DashboardApp.exe)
-set(__QT_DEPLOY_TARGET_DashboardApp_RUNTIME_DLLS C:/Qt/6.7.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
